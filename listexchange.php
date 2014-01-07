e549cd1b509a5c91174a14ffb167e2d89ab56a7a
joekeyel <hasanul2307@gmail.com>
2014-01-08 00:31:43 +0800

    Update listexchange.php

--- listexchange.php
+++ listexchange.php
@@ -5,9 +5,7 @@ header("Content-Type: text/html");
 
 $url = "http://58.27.84.166/mcconline/migrate/main_index.php";
 $result = file_get_contents($url);
-$firstrow=array();
-$firstrow=explode('<br>',$result);
-/* Check response status */
+
 list($version,$status,$msg) = explode(' ',$http_response_header[0], 3);
 if ($status != 200)
 echo "Your call to the web service returned an error status of $status.";
